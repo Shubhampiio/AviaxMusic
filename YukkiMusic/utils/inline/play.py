4048db7bb0ab055457a7079483988258f5068675
Hari Singh <121376839+Lover-Music@users.noreply.github.com>
2023-01-22 13:57:41 +0530

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -67,8 +67,8 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="🏠 Support",
-                url="https://telegram.dog/AviaxSupport",
+                text="💫 Support",
+                url="https://t.me/the_chatting",
             ),
             InlineKeyboardButton(
                 text=_["PL_B_3"],
@@ -122,8 +122,8 @@ def stream_markup(_, videoid, chat_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🏠 Support",
-                url="https://telegram.dog/AviaxSupport",
+                text="💫 Support",
+                url="https://t.me/the_chatting",
             ),
             InlineKeyboardButton(
                 text=_["PL_B_3"],
